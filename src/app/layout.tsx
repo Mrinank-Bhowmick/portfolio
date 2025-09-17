2d342561903e725ceb6616a53d4fec913ee21007
Mrinank-Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2025-09-17 21:11:58 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Sidebar from "@/components/sidebar";
@@ -34,6 +35,7 @@ export default function RootLayout({
             <Sidebar />
           </div>
           {children}
+          <Analytics />
         </div>
       </body>
     </html>
