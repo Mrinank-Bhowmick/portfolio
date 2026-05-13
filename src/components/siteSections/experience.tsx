a6b25d0f080deeb7aabb83dd9611da36ac6d8eac
Mrinank-Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2026-05-13 22:11:23 +0530

    updated exp

--- src/components/siteSections/experience.tsx
+++ src/components/siteSections/experience.tsx
@@ -4,7 +4,7 @@ const Experience = () => {
       company: "Datacurve AI (YC W24)",
       location: "Remote/US",
       role: "Code Eval Team",
-      duration: "Dec 2025 - Present",
+      duration: "Dec 2025 - Apr 2026",
       experience: [
         "Review and evaluate developer-submitted code for correctness, originality, and adherence to platform guidelines.",
         "Provide detailed technical feedback, enforce submission standards, and maintain high-quality evaluation consistency across reviews.",
